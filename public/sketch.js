68ea0dcb81f51115e7a9c76e84fee9fa9e2cc51a
andreas schlegel <andi@sojamo.de>
2020-05-23 18:15:09 +0800

    Update visual and control

--- public/sketch.js
+++ public/sketch.js
@@ -1,7 +1,13 @@
 let body = {};
 let ry = 0,
   x = 0,
-  y = 0;
+  y = 0,
+  z = 0,
+  nry = 0,
+  nx = 0,
+  ny = 100,
+  nz = 0;
+
 let isMove = false;
 
 function setup() {
@@ -11,18 +17,38 @@ function setup() {
 }
 
 function draw() {
-  /* update skeleton details first */
-  updateSkeleton();
+  /* update body details first */
+  updateBody();
 
-  /* then draw workd and skeleton */
+  /* then draw workd and body */
   background(0);
   lights();
   noStroke();
-  fill(255);
+
   push();
-  translate(x, y);
+  translate(x, y, z);
   rotateY(ry);
+
+  /** floor-grid */
+  push();
+  rotateX(HALF_PI);
+  let cx = 4;
+  let cy = 4;
+  let cs = 200;
+  translate((-cs * cx) / 2, (-cs * cy) / 2);
+
+  for (let x = 0; x < cx; x++) {
+    for (let y = 0; y < cy; y++) {
+      fill(255, x % 2 === 0 ? (y % 2 === 0 ? 30 : 60) : y % 2 === 0 ? 60 : 30);
+      rect(cs * x, cs * y, cs, cs);
+    }
+  }
+
+  pop();
+
+  /** Bodies */
   Object.keys(body).forEach((id) => {
+    fill(255);
     push();
     Object.keys(body[id].now).forEach((joint) => {
       push();
@@ -32,15 +58,22 @@ function draw() {
       pop();
     });
     pop();
+    translate(-200, 0, 0);
+    rotateY(HALF_PI);
   });
   pop();
+
+  x += (nx - x) * 0.1;
+  y += (ny - y) * 0.1;
+  z += (nz - z) * 0.1;
+  ry += (nry - ry) * 0.1;
 }
 
 function keyPressed() {
   /* check if Shift (keyCode 16) key is pressed
-   * if so, we are then able to move the skeleton
+   * if so, we are then able to move the body
    * with a mouseDrag, otherwise the mousedrag
-   * will affect the rotation of the skeleton,
+   * will affect the rotation of the body,
    * see mouseDragged() below.
    */
   isMove = keyCode == 16 ? true : isMove;
@@ -53,15 +86,21 @@ function keyReleased() {
 function mouseDragged() {
   if (isMove) {
     // move along x and y axis
-    x += mouseX - pmouseX;
-    y += mouseY - pmouseY;
+    nx += mouseX - pmouseX;
+    ny += mouseY - pmouseY;
   } else {
     // rotate around Y-axis
-    ry += (pmouseX - mouseX) * 0.1;
+    nry += (mouseX - pmouseX) * 0.02;
   }
 }
 
-function updateSkeleton() {
+function mouseWheel(event) {
+  // event.preventDefault();
+  nz += event.delta;
+  return false;
+}
+
+function updateBody() {
   /*
    * since data is coming in at a lower update rate
    * than our framerate, lets interpolate between
