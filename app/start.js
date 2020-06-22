05305a310da150fa72bf639ea78257fd8200ba61
andreas schlegel <andi@sojamo.de>
2020-06-22 18:01:06 +0800

    Rework and add help

--- app/start.js
+++ app/start.js
@@ -1,50 +1,72 @@
 import Load from './load';
 import Stream from './stream';
+import StreamLoad from './streamload';
 import Settings from '../src/Settings';
 import { log } from '../src/Log';
 
 const fs = require('fs');
 const argv = require('minimist')(process.argv.slice(2));
 let settings;
+let isHelp = false;
 
 Object.keys(argv).forEach((key) => {
   const val = argv[key];
   switch (key) {
     case 'settings':
-      const pathToSettings = '../external/settings/';
-      const fileName = val === true ? 'settings-todo.json' : val;
+      const pathToSettings = '../external/storage/settings/';
+      const fileName = val === true ? 'settings-template.json' : val;
       settings = new Settings(JSON.parse(fs.readFileSync(pathToSettings + fileName, 'utf-8')));
+      break;
+    case 'help':
+      isHelp = true;
       break;
   }
 });
 
-settings = settings || new Settings(Settings.default);
-log.info(`app start: Settings are ${settings.label}`);
+if (isHelp) {
+  console.log(
+    `
+  Usage: yarn run with [options]
 
-if (argv['app'] !== undefined) {
-  switch (argv['app']) {
-    case 'stream':
-      log.info('app start: App selected is stream, starting with Stream class.');
-      new Stream(settings);
-      break;
-    case 'load':
-      log.info('app start: App selected is load, starting with Load class.');
-      new Load(settings);
-      break;
-    case 'sim':
-      /** TODO
-       * new Sim(options);
-       **/
+  Options:
+    --mode [ load | stream | streamload ]
+    --settings [ omit | empty | filename ]
+    --help
 
-      break;
-    default:
-      log.info(
-        `app start: App selected is unknown ${argv['app']} starting with Load class as default.`,
-      );
-      new Load(settings);
-      break;
-  }
+  Examples:
+    yarn run with --mode load
+    yarn run with --mode stream
+    yarn run with --mode streamload
+    yarn run with --mode streamload --settings settings-template.json
+    
+  `,
+  );
 } else {
-  log.info(`app start: No app selected, starting with Load class as default.`);
-  new Load(settings);
+  settings = settings || new Settings(Settings.default);
+  log.info(`Start: Settings are ${settings.label}`);
+
+  if (argv['mode'] !== undefined) {
+    switch (argv['mode']) {
+      case 'stream':
+        log.info('Start: Mode selected is stream, starting with Stream class.');
+        new Stream(settings);
+        break;
+      case 'load':
+        log.info('Start: Mode selected is load, starting with Load class.');
+        new Load(settings);
+        break;
+      case 'streamload':
+        log.info('Start: Mode selected is streamload, starting with StreamLoad class.');
+        new StreamLoad(settings);
+        break;
+      case 'sim': /** TODO new Sim(options); */
+      default:
+        log.info(`Start: Mode selected is unknown ${argv['mode']} starting with Load class as default.`);
+        new Load(settings);
+        break;
+    }
+  } else {
+    log.info(`Start: No Mode selected, starting with Load class as default.`);
+    new Load(settings);
+  }
 }
